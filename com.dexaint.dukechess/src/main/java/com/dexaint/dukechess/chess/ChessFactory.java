8b771b46a263d61fa3d40d57dbcc33d5766397c1
Dexaint <zjbthomas@qq.com>
2015-12-20 18:11:55 +0800

    Use xml to create chesses

--- com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/chess/ChessFactory.java
+++ com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/chess/ChessFactory.java
@@ -1,40 +1,23 @@
 package com.dexaint.dukechess.chess;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
 import com.dexaint.dukechess.flow.Player;
 
 public class ChessFactory {
+	private Document document;
 	
-	public static Chess createChess(ChessType type, Player p) {
-		switch (type)
-		{
-		case Assassin:
-			return new Assassin(p);
-		case Bowman:
-			return new Bowman(p);
-		case Champion:
-			return new Champion(p);
-		case Dragoon:
-			return new Dragoon(p);
-		case Duke:
-			return new Duke(p);
-		case Footman:
-			return new Footman(p);
-		case General:
-			return new General(p);
-		case Knight:
-			return new Knight(p);
-		case Pikeman:
-			return new Pikeman(p);
-		case Priest:
-			return new Priest(p);
-		case Ranger:
-			return new Ranger(p);
-		case Seer:
-			return new Seer(p);
-		case Wizard:
-			return new Wizard(p);	
-		default:
-			return null;
+	public ChessFactory() {
+		try {
+			this.document = new SAXReader().read(ChessFactory.class.getResourceAsStream("Chess.xml"));
+		} catch (DocumentException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public Chess createChess(ChessType type, Player p) {
+		return new ChessImpl(p, type, this.document);
 	}	
 }
--- com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/chess/ChessImpl.java
+++ com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/chess/ChessImpl.java
@@ -1,9 +1,11 @@
 package com.dexaint.dukechess.chess;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
-
 import org.apache.commons.lang3.ArrayUtils;
+import org.dom4j.Document;
+import org.dom4j.Element;
 
 import com.dexaint.dukechess.action.ActionType;
 import com.dexaint.dukechess.movement.Destination;
@@ -11,14 +13,44 @@ import com.dexaint.dukechess.movement.MovementType;
 import com.dexaint.dukechess.flow.Field;
 import com.dexaint.dukechess.flow.Player;
 
-public abstract class ChessImpl implements Chess{
+public class ChessImpl implements Chess{
 	public boolean starter = true;
 	private Player player;
+	private ChessType chessType;
+	private Element chessRoot;
 	private ActionType[] actions;
 	
-	protected ChessImpl(Player player, ActionType[] actions) {
+	@SuppressWarnings("unchecked")
+	protected ChessImpl(Player player, ChessType chessType, Document document) {
 		this.player = player;
-		this.actions = actions;
+		this.chessType = chessType;
+
+		List<Element> nameList = document.getRootElement().elements("name");
+		for (Element name : nameList) {
+			if (name.getText().equals(chessType.toString())) {
+				this.chessRoot = name.getParent(); // Back to <chess/>
+				this.actions = (ActionType[]) this.chessRoot.element("actions").elements("action").toArray();
+				break;
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public HashMap<Destination, MovementType> getStyle(ActionType action) {
+		List<Element> styleList = this.chessRoot.element("styles").elements("style");
+		HashMap<Destination, MovementType> ret = new HashMap<Destination, MovementType>();
+		for (Element style : styleList) {
+			if (style.element("action").getText().equals(action.toString()) &&
+					(this.starter?"1":"0").equals(style.element("starter").getText())) {
+				List<Element> targetList = style.element("targets").elements("target");
+				for (Element target : targetList) {
+					ret.put(Destination.valueOf(target.element("destination").getText()),
+						MovementType.valueOf(target.element("movement").getText()));
+				}
+				return ret;
+			}
+		}
+		return new HashMap<Destination, MovementType>();
 	}
 	
 	public ActionType[] getAvailableActions(Field field, int pos) {
@@ -42,7 +74,7 @@ public abstract class ChessImpl implements Chess{
 		return ret;
 	}	
 	
-	public abstract HashMap<Destination, MovementType> getStyle(ActionType action);
+	//public abstract HashMap<Destination, MovementType> getStyle(ActionType action);
 	
 	public Player getPlayer()
 	{
