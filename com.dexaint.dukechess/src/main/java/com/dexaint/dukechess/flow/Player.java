396262e539fdf044243e8fe93ad2638ddc755bae
Dexaint <zjbthomas@qq.com>
2015-12-20 15:12:51 +0800

    Change Player.java
    Change Player.java with properties files

--- com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/flow/Player.java
+++ com.dexaint.dukechess/src/main/java/com/dexaint/dukechess/flow/Player.java
@@ -1,40 +1,37 @@
 package com.dexaint.dukechess.flow;
 
 import java.util.LinkedList;
+import java.util.Properties;
+
 import com.dexaint.dukechess.chess.ChessType;
 
 public class Player {
 	private int index;
-	public int getIndex(){return index;}
 	private int direction;
-	public int getDirection(){return direction;}
-	private static LinkedList<ChessType>  chessList = new LinkedList<ChessType>();
-	public LinkedList<ChessType> getChessList(){return chessList;}
+	private LinkedList<ChessType> chessList = new LinkedList<ChessType>();
 	
 	public Player(int index, int direction)
 	{
 		this.index=index;
 		this.direction=direction;
+		
+		Properties props = new Properties();
+		try {
+			props.load(Player.class.getResourceAsStream("Player.properties"));
+			String[] chesses = props.get("chesses").toString().split(",");
+			for (String chess : chesses) {
+				chessList.add(ChessType.valueOf(chess));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	static {
-		chessList.add(ChessType.Assassin);
-		chessList.add(ChessType.Bowman);
-		chessList.add(ChessType.Champion);
-		chessList.add(ChessType.Dragoon);
-		chessList.add(ChessType.Duke);
-		chessList.add(ChessType.Footman);
-		chessList.add(ChessType.Footman);
-		chessList.add(ChessType.Footman);
-		chessList.add(ChessType.General);
-		chessList.add(ChessType.Knight);
-		chessList.add(ChessType.LongBowman);
-		chessList.add(ChessType.Marshall);
-		chessList.add(ChessType.Pikeman);
-		chessList.add(ChessType.Pikeman);
-		chessList.add(ChessType.Pikeman);
-		chessList.add(ChessType.Priest);
-		chessList.add(ChessType.Ranger);
-		chessList.add(ChessType.Seer);
-		chessList.add(ChessType.Wizard);
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public int getDirection() {
+		return direction;
 	}
 }
